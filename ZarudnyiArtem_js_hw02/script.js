77c9845cc82d9140373a5c82e1651b3f55d4b0e5
Artem Zarudnyi <142827893+artez10@users.noreply.github.com>
2023-10-17 20:19:06 +0200

    Add files via upload

---
+++ ZarudnyiArtem_js_hw02/script.js
@@ -0,0 +1,18 @@
+// 1
+// let userName = prompt ("What is your Name?");
+// let userAge = prompt ("How old are you?");
+// let userOrigin = prompt ("Where are you from?");
+
+// document.write(`${(userName + " " +userAge+ " " +userOrigin)}`)
+
+// 2
+// let numb = +prompt("enter a five-digit number");
+// while(numb < 10000 || numb > 99999) {
+//         numb = +prompt("enter a five-digit number");
+// }
+// console.log(numb); 
+
+// let result = numb.toString().split('');
+
+// console.log(result);
+// console.log(result. join(''));
